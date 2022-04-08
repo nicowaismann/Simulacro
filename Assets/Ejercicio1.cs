bc1583b406c9482a70eb139748b277810b5fd71d
nicowaismann <46961446@est.ort.edu.ar>
2022-04-08 08:33:50 -0300

    Initial commit

---
+++ Assets/Ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+
+    public int primerValor;
+    public int segundoValor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (primerValor == segundoValor)
+        {
+            Debug.Log("Son iguales");
+        }
+
+        else
+        {
+            Debug.Log("No son iguales");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicio2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float capitalDisponible;
+    float precioTotal;
+    float capitalSobrante;
+    float capitalFaltante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        precioTotal = precio1 + precio2 + precio3;
+        capitalSobrante = capitalDisponible - precioTotal;
+        capitalFaltante = (capitalDisponible - precioTotal) * (-1); 
+
+        if (precioTotal > capitalDisponible)
+        {
+            Debug.Log("El precio supera al capital disponible y te faltan " + capitalFaltante);
+        }
+
+        else
+        {
+            Debug.Log("El precio no supera al capital disponible y te sobran " + capitalSobrante);
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
